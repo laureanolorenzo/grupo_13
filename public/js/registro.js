9b513fea2d5092159d785680b8a778640f5a083e
Gonza-Zarlenga <gzarlenga95@gmail.com>
2024-04-02 16:46:47 -0300

    javascript para front

---
+++ public/js/registro.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", () => {
+    let form = document.querySelector(".form");
+    console.log(form);
+    console.log(form.username)
+
+    // form.addEventListenner("submit", (event) => {
+
+    //     if (form.name.value == "") {
+    //         console.log("el nombre está vacío")
+    //     }
+    // }
+    // )
+})
+
